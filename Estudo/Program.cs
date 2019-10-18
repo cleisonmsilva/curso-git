04df619527b59c97358ec6f7d59d3aefb5bc3b51
Cleison <cleison.msilva@hotmail.com>
2019-10-17 21:37:16 -0300

    Fase1

---
+++ Estudo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("bom dia!");
+            Console.WriteLine("bom tarde!");
+            Console.WriteLine("Boa noite!");
+        }
+    }
+}
